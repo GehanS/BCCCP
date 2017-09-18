3d9f71e92a9346cde7d1d66cdb3abd1947425f6e
longdd1997 <30396806+longdd1997@users.noreply.github.com>
2017-09-18 10:15:49 +1000

    Update Gate.java

--- carpark/Gate.java
+++ carpark/Gate.java
@@ -4,10 +4,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JTextField;
-import java.awt.Font;
-import javax.swing.SwingConstants;
+
 import java.awt.Color;
 
 @SuppressWarnings("serial")
