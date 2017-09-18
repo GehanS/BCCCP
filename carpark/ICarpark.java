d8aea25cac1c48ed71cbaf24a2f871c55f8abad5
longdd1997 <30396806+longdd1997@users.noreply.github.com>
2017-09-18 10:16:12 +1000

    Update ICarpark.java

--- carpark/ICarpark.java
+++ carpark/ICarpark.java
@@ -9,20 +9,3 @@ public interface ICarpark {
 	public void deregister(ICarparkObserver observer);
 	public String getName();
 	public boolean isFull();
-	
-	public IAdhocTicket issueAdhocTicket();
-	public void recordAdhocTicketEntry();
-	public IAdhocTicket getAdhocTicket(String barcode);
-	public float calculateAddHocTicketCharge(long entryDateTime);
-	public void recordAdhocTicketExit();
-	
-	public void registerSeasonTicket(ISeasonTicket seasonTicket);
-	public void deregisterSeasonTicket(ISeasonTicket seasonTicket);
-
-	public boolean isSeasonTicketValid(String ticketId);
-	public boolean isSeasonTicketInUse(String ticketId);
-	public void recordSeasonTicketEntry(String ticketId);
-	public void recordSeasonTicketExit(String ticketId);
-
-
-}
